a866f9cfcfb26e1a2191b3c35ae43a8060e79028
Bartosz Leśniak <bartek.lesniak.2006@gmail.com>
2026-03-10 00:25:07 +0100

    Zrobione zadanie dodatkowe- lista dwukierunkowa

--- lab2/lab2dodatkowe.py
+++ lab2/lab2dodatkowe.py
@@ -1,22 +1,33 @@
-
 class Elem:
     def __init__(self, data):
         self.data = data
         self.next = None
+        self.prev = None
 
 class List:
     def __init__(self):
         self.head = None
+        self.tail = None
     
     def destroy(self):
+        current = self.head
+        while current is not None:
+            next_elem = current.next
+            current.prev = None
+            current.next = None
+            current = next_elem
         self.head = None
+        self.tail = None
     
     def add(self, data):
         new_elem = Elem(data)
         if self.head is None: 
             self.head = new_elem
+            self.tail = new_elem
         else:
             new_elem.next = self.head
+            new_elem.prev = None
+            self.head.prev = new_elem
             self.head = new_elem
     
     def append(self,data):
@@ -24,35 +35,39 @@ class List:
 
         if self.head is None:
             self.head = last_elem
+            self.tail = last_elem
         else:
-            current = self.head
-            while current.next is not None:
-                current = current.next
-            
-            current.next = last_elem
+            last_elem.prev = self.tail
+            last_elem.next = None
+            self.tail.next = last_elem
+            self.tail = last_elem
 
     def remove(self):
         if self.head is None:
             return None
-        
-        removed = self.head
-        self.head = self.head.next
-        return removed.data
-            
+        elif self.head == self.tail:
+            removed = self.head
+            self.head = None
+            self.tail = None
+            return removed.data
+        else:
+            removed = self.head
+            self.head = self.head.next
+            self.head.prev = None
+            return removed.data
 
     def remove_end(self):
         if self.head is None:
             return None
-        elif self.head.next is None:
+        elif self.head == self.tail:
             removed = self.head
             self.head = None
+            self.tail = None
             return removed.data
         else:
-            current = self.head
-            while current.next.next is not None:
-                current = current.next
-            removed = current.next
-            current.next = None
+            removed = self.tail
+            self.tail = self.tail.prev
+            self.tail.next = None
             return removed.data
     
     def is_empty(self):
@@ -83,6 +98,17 @@ class List:
 
         return result
 
+    def print_list_reverse(self):
+        current = self.tail
+        result = ""
+
+        while current:
+            result += f"-> {current.data}\n"
+            current = current.prev
+        return result
+    
+    
+
 if __name__ == "__main__":
 
     dane = [
@@ -109,6 +135,8 @@ if __name__ == "__main__":
     
     print("Lista uczelni:")
     print(uczelnie.print_list())
+    print("Lista uczelni od tyłu")
+    print(uczelnie.print_list_reverse())
 
    
     print("Długość listy", uczelnie.length())
@@ -125,6 +153,8 @@ if __name__ == "__main__":
     
     print("Lista po usunięciu pierwszego i ostatniego elementu")
     print(uczelnie.print_list())
+    print("Lista po usunięciu pierwszego i ostatniego elementu od tyłu")
+    print(uczelnie.print_list_reverse())
 
    
     uczelnie.destroy()
